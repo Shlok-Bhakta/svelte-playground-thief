333608c87e83f701ef2b366ae5452be3cd3d3e77
Shlok Bhakta <shlokbhakta1@gmail.com>
2024-12-09 23:34:19 +0000

    Start creating catppuccin theme

--- packages/repl/src/lib/Repl.svelte
+++ packages/repl/src/lib/Repl.svelte
@@ -208,7 +208,7 @@
 		flex: 1;
 		height: 100%;
 		min-height: 0;
-		background: var(--sk-bg-1);
+		background: #1e1e2e;
 		padding: 0;
 
 		&.embedded {
@@ -273,5 +273,106 @@
 				cursor: default;
 			}
 		}
+	}
+
+	:global .ͼo {
+		color: #cba6f7;
+	}
+
+	:global .ͼq {
+		color: #89b4fa;
+	}
+
+	:global .ͼt {
+		color: #89b4fa;
+	}
+
+	:global .ͼp{
+		color: #f9e2af;
+	}
+
+	:global .ͼu{
+		color: #cdd6f4;
+	}
+
+	:global .ͼv{
+		color: #9399b2;
+	}
+
+	:global .error{
+		background-color: #11111b !important;
+	}
+
+	@font-face {
+		font-family: "CaskaydiaCoveNF";
+		src: url("/fonts/CaskCov.woff2");
+	}
+
+	.nerdfont {
+		font-family: "CaskaydiaCoveNF" !important;
+	}
+	:global .cm-editor{
+		font-family: "CaskaydiaCoveNF" !important;
+	}
+
+	:global .cm-gutters{
+		font-family: "CaskaydiaCoveNF" !important;
+		background-color: #1e1e2e !important;
+		color: #cdd6f4 !important;
+	}
+
+	:global .button{
+		color: #cdd6f4 !important;
+		font-family: "CaskaydiaCoveNF" !important;
+	}
+
+	:global .drag-handle{
+		color: #cdd6f4 !important;
+	}
+
+	:global .container{
+		color: #181825 !important;
+	}
+
+	:global pre{
+		color: #cdd6f4 !important;
+	}
+
+	:global .overlay{
+		background-color: #6c7086 !important;
+		
+	}
+
+	:global .cm-content{
+		font-family: "CaskaydiaCoveNF" !important;
+	}
+
+	:global :root{
+		font-family: "CaskaydiaCoveNF" !important;
+	}
+
+	:global .dropdown{
+		color: #cdd6f4 !important;
+		font-family: "CaskaydiaCoveNF" !important;
+	}
+
+	:global .cm-layer{
+		color: #6c7086 !important;
+	}
+
+	:global .view-toggle{
+		color: #cdd6f4 !important;
+	}
+
+	:global .filename{
+		font-family: "CaskaydiaCoveNF" !important;
+	}
+
+	:global body{
+		font-family: "CaskaydiaCoveNF" !important;
+	}
+
+	:global input{
+		font-family: "CaskaydiaCoveNF" !important;
 	}
 </style>
